edac6173db8fcb3c9f06768c7006276908f1ee02
yazid Elhachimi <elhachimiyazid@gmail.com>
2021-09-03 01:40:12 +0200

    ajout movies app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviePopularListComponent } from './movie-popular-list/movie-popular-list.component';
+import { MovieNowplayingListComponent } from './movie-nowplaying-list/movie-nowplaying-list.component';
+import {HttpClientModule} from '@angular/common/http';
+import { MoviePopularListAfficheComponent } from './movie-popular-list/movie-popular-list-affiche/movie-popular-list-affiche.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AfficheDetailsComponent } from './movie-popular-list/movie-popular-list-affiche/affiche-details/affiche-details.component';
+import { MovieNowplayingListAfficheComponent } from './movie-nowplaying-list/movie-nowplaying-list-affiche/movie-nowplaying-list-affiche.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviePopularListComponent,
+    MovieNowplayingListComponent,
+    MoviePopularListAfficheComponent,
+    AfficheDetailsComponent,
+    MovieNowplayingListAfficheComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
